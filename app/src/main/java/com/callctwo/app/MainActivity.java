58b7b5427ed5922d22a234b5ff99dbda7bad4ea1
Jānis <janis.indans@gmail.com>
2014-06-13 10:06:46 +0300

    Initial commit

---
+++ app/src/main/java/com/callctwo/app/MainActivity.java
@@ -0,0 +1,42 @@
+package com.callctwo.app;
+
+import android.app.Activity;
+// import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator_four);
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        /* if (id == R.id.clear_history) {
+            return true;
+        }
+        */
+        return id == R.id.clear_history || super.onOptionsItemSelected(item);
+        //return super.onOptionsItemSelected(item);
+    }
+
+
+}
